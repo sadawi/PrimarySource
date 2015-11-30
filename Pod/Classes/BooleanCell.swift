70b0f0118b0773d21bd61584fd783d91e45bcd2f
Sam Williams <samuel.williams@gmail.com>
2015-11-30 18:26:40 -0500

    cleanup bool cell

--- Pod/Classes/BooleanCell.swift
+++ Pod/Classes/BooleanCell.swift
@@ -10,27 +10,15 @@ import UIKit
 
 public class BooleanCell:FieldCell {
     // TODO: to be consistent, probably should be (Bool?)
-    public var value:Bool = false
+    public var value:Bool? = false
 }
 
 public class SwitchCell:BooleanCell, TappableTableCell  {
     var switchControl:UISwitch?
     
-    // TODO: probably want to leave the var alone, and add some overridable methods
-    public override var value:Bool {
-        get {
-            return self.switchControl?.on ?? false
-        }
-        set {
-            self.switchControl?.on = newValue
-        }
-    }
-    
     override func buildView() {
         super.buildView()
         let control = UISwitch(frame: self.controlView!.bounds)
-        
-        //        control.setContentCompressionResistancePriority(UILayoutPriorityRequired, forAxis: UILayoutConstraintAxis.Vertical)
         
         self.addControl(control, alignment:.Right)
         control.addTarget(self, action: Selector("valueChanged"), forControlEvents: UIControlEvents.ValueChanged)
@@ -38,8 +26,13 @@ public class SwitchCell:BooleanCell, TappableTableCell  {
     }
     
     public func toggle(animated:Bool=true) {
-        self.switchControl?.setOn(!self.value, animated: animated)
+        self.value = (self.value != true)
         self.valueChanged()
+    }
+    
+    public override func update() {
+        super.update()
+        self.switchControl?.on = (self.value == true)
     }
     
     func handleTap() {
