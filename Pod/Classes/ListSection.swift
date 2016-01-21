b573994081eaa03d216cfd77772ea21ca599918b
Sam Williams <samuel.williams@gmail.com>
2016-01-20 21:30:07 -0800

    comments

--- Pod/Classes/ListSection.swift
+++ Pod/Classes/ListSection.swift
@@ -8,14 +8,24 @@
 
 import Foundation
 
+/**
+ A collection section that is built from a list of objects.
+ */
 public class ListSection<T:Equatable>: Section {
     public typealias ItemGenerator = ((T, Int) -> CollectionItem)
     
     var values: [T] = []
     var generator: ItemGenerator
     
-    public init(values:[T], title: String?=nil, key: String?=nil, reorderable: Bool?=nil, generator:ItemGenerator) {
-        self.values = values
+    /**
+     - parameter values: An array of item values from which to build this section.
+     - parameter title: Section header title
+     - parameter key: A unique identifier for this section, used to look up the section from its datasource.
+     - parameter reorderable: Reorderability flag
+     - parameter generator: A closure that generates a collection item from a value.
+     */
+    public init(values:[T]?, title: String?=nil, key: String?=nil, reorderable: Bool?=nil, generator:ItemGenerator) {
+        self.values = values ?? []
         self.generator = generator
         super.init(title: title, key: key, reorderable: reorderable)
         self.generateItems()
@@ -33,6 +43,9 @@ public class ListSection<T:Equatable>: Section {
         return self.generator(value, index)
     }
 
+    /**
+     Removes a value from this section.
+     */
     public func removeValueAtIndex(index:Int, updateView: Bool = false) {
         self.values.removeAtIndex(index)
         self.items.removeAtIndex(index)
@@ -43,12 +56,24 @@ public class ListSection<T:Equatable>: Section {
         }
     }
     
+    /**
+     Removes a value from this section.
+     
+     - parameter value: The value to be removed
+     - parameter updateView: Whether the value's corresponding item should be removed from the view
+     */
     public func removeValue(value:T, updateView: Bool = false) {
         if let index = self.values.indexOf(value) {
             self.removeValueAtIndex(index, updateView: updateView)
         }
     }
     
+    /**
+     Appends a value to this section.
+     
+     - parameter value: A new value
+     - parameter updateView: Whether the value's corresponding item should be added to the view
+     */
     public func addValue(value: T, updateView: Bool = false) -> Section {
         self.values.append(value)
         
