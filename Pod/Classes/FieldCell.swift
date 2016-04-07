ae919aed653a9e1db95ce5d956988944fe72f549
Sam Williams <samuel.williams@gmail.com>
2016-04-07 14:13:27 -0400

    inserted TitleDetailsCell > FieldCell

--- Pod/Classes/FieldCell.swift
+++ Pod/Classes/FieldCell.swift
@@ -23,14 +23,9 @@ public enum FieldState {
     case Editing
 }
 
-public class FieldCell: TableCell {
-    var showTitleLabel:Bool = true
-    var titleLabel:UILabel?
+public class FieldCell: TitleDetailsCell {
     var errorLabel:UILabel?
     var errorIcon:UIImageView?
-    
-    var mainContent:UIView?
-    var detailContent:UIView?
     
     public var readonly:Bool = false
     
@@ -40,14 +35,10 @@ public class FieldCell: TableCell {
         }
     }
     
-    public var controlView:UIStackView?
-    
     public var blank:Bool {
         get { return true }
     }
     
-    var contentConstraints:[NSLayoutConstraint] = []
-
     public var state:FieldState = .Normal {
         didSet {
             self.update()
@@ -55,11 +46,9 @@ public class FieldCell: TableCell {
         }
     }
     
-    public dynamic var titleTextColor:UIColor? = UIColor.blackColor()
     public dynamic var errorTextColor:UIColor? = UIColor.redColor()
     
     public dynamic var valueTextColor:UIColor? = UIColor(white: 0.4, alpha: 1)
-    public dynamic var titleFont:UIFont = UIFont.boldSystemFontOfSize(17)
     public dynamic var valueFont:UIFont = UIFont.systemFontOfSize(17)
     
     lazy var accessoryToolbar:UIToolbar = {
@@ -102,62 +91,18 @@ public class FieldCell: TableCell {
         self.update()
     }
     
-    public var labelPosition:FieldLabelPosition = .Left {
-        didSet {
-            self.setupConstraints()
-            self.stylize()
-        }
-    }
-    
     public var onChange:(Void -> Void)?
-    
-    public var title:String? {
-        didSet {
-            self.update()
-        }
-    }
-    
-    func formattedTitle() -> String? {
-        if self.labelPosition == .Top {
-            return self.title?.uppercaseString
-        } else {
-            return self.title
-        }
-    }
     
     override public func buildView() {
         super.buildView()
         
-        let mainContent = UIView(frame: CGRect.zero)
-        mainContent.translatesAutoresizingMaskIntoConstraints = false
-        self.contentView.addSubview(mainContent)
-        self.mainContent = mainContent
-
-        let detailContent = UIView(frame: CGRect.zero)
-        detailContent.translatesAutoresizingMaskIntoConstraints = false
-        self.contentView.addSubview(detailContent)
-        self.detailContent = detailContent
-        
-        let titleLabel = UILabel(frame: CGRect.zero)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        self.mainContent?.addSubview(titleLabel)
-        self.titleLabel = titleLabel
-
-        let errorLabel = UILabel(frame: detailContent.bounds)
+        let errorLabel = UILabel(frame: self.detailContent!.bounds)
         errorLabel.numberOfLines = 0
         errorLabel.translatesAutoresizingMaskIntoConstraints = false
         self.detailContent?.addSubview(errorLabel)
         self.errorLabel = errorLabel
 
-        let controlView = UIStackView(frame: CGRect.zero)
-        controlView.axis = UILayoutConstraintAxis.Horizontal
-        controlView.translatesAutoresizingMaskIntoConstraints = false
-        controlView.spacing = 15
-        self.mainContent?.addSubview(controlView)
-        self.controlView = controlView
-        
-        
-        let views = ["error":errorLabel, "main":mainContent, "detail":detailContent]
+        let views = ["error":errorLabel, "main":self.mainContent!]
         let metrics = [
             "left": self.defaultContentInsets.left,
             "right": self.defaultContentInsets.right,
@@ -169,21 +114,11 @@ public class FieldCell: TableCell {
         self.detailContent?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[error]-right-|", options: .AlignAllTop, metrics: metrics, views: views))
         
         self.detailContent?.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[error]|", options: .AlignAllTop, metrics: metrics, views: views))
-        
-        var constraints:[NSLayoutConstraint] = []
-        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[main]|", options: .AlignAllTop, metrics: metrics, views: views)
-        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[detail]|", options: .AlignAllTop, metrics: metrics, views: views)
-        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|[main][detail]-bottom-|", options: .AlignAllLeft, metrics: metrics, views: views)
-        self.contentView.addConstraints(constraints)
-    
-        
-        self.setupConstraints()
-        self.stylize()
-        self.update()
     }
     
-    func update() {
-        self.titleLabel?.text = self.formattedTitle()
+    override func update() {
+        super.update()
+
         switch self.state {
         case .Error(let messages):
             self.errorIcon?.image = UIImage(named: "error-32", inBundle: NSBundle(forClass: FieldCell.self), compatibleWithTraitCollection: nil)?.imageWithRenderingMode(.AlwaysTemplate)
@@ -197,52 +132,9 @@ public class FieldCell: TableCell {
     override public func stylize() {
         super.stylize()
         
-        switch self.labelPosition {
-        case .Left:
-            self.titleLabel?.font = self.titleFont
-            self.titleLabel?.textColor = self.titleTextColor
-        case .Top:
-            self.titleLabel?.textColor = UIColor(white: 0.5, alpha: 1)
-            self.titleLabel?.font = UIFont.boldSystemFontOfSize(11)
-        }
-        
         self.errorLabel?.textColor = self.errorTextColor
         self.errorIcon?.tintColor = self.errorTextColor
         self.errorLabel?.font = self.valueFont
-    }
-    
-    func setupConstraints() {
-        guard let titleLabel=self.titleLabel, controlView=self.controlView, mainContent=self.mainContent else { return }
-        
-        let views = ["title":titleLabel, "controls":controlView, "main":mainContent]
-        let metrics = [
-            "left": self.defaultContentInsets.left,
-            "right": self.defaultContentInsets.right,
-            "top": self.defaultContentInsets.top,
-            "bottom": self.defaultContentInsets.bottom,
-            "controlHeight": 10
-        ]
-        
-        for constraint in self.contentConstraints {
-            self.mainContent?.removeConstraint(constraint)
-        }
-        
-        var newConstraints:[NSLayoutConstraint] = []
-        
-        switch self.labelPosition {
-        case .Left:
-            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[title]-[controls]-right-|", options: .AlignAllTop, metrics: metrics, views: views)
-            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-top-[title]|", options: .AlignAllTop, metrics: metrics, views: views)
-            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-top-[controls]|", options: .AlignAllTop, metrics: metrics, views: views)
-        case .Top:
-            let options = NSLayoutFormatOptions.AlignAllLeft
-            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[title]-right-|", options: options, metrics: metrics, views: views)
-            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[controls]-right-|", options: options, metrics: metrics, views: views)
-            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-top-[title]-[controls(>=controlHeight)]-|", options: options, metrics: metrics, views: views)
-        }
-        
-        self.mainContent?.addConstraints(newConstraints)
-        self.contentConstraints = newConstraints
     }
     
     func valueChanged() {
@@ -253,15 +145,5 @@ public class FieldCell: TableCell {
         if let onChange = self.onChange {
             onChange()
         }
-    }
-    
-    func addControl(control:UIView, alignment:ControlAlignment = .Right) {
-        control.translatesAutoresizingMaskIntoConstraints = false
-        self.controlView!.addArrangedSubview(control)
-    }
-    
-    override public func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        self.stylize()
     }
 }
---
+++ Pod/Classes/TitleValueCell.swift
@@ -0,0 +1,153 @@
+//
+//  TitleValueCell.swift
+//  Pods
+//
+//  Created by Sam Williams on 4/7/16.
+//
+//
+
+import UIKit
+
+public class TitleDetailsCell: TableCell {
+    var showTitleLabel:Bool = true
+    var titleLabel:UILabel?
+    
+    var mainContent:UIView?
+    var detailContent:UIView?
+    
+    public var controlView:UIStackView?
+    
+    var contentConstraints:[NSLayoutConstraint] = []
+    
+    public dynamic var titleTextColor:UIColor? = UIColor.blackColor()
+    public dynamic var titleFont:UIFont = UIFont.boldSystemFontOfSize(17)
+
+    public var labelPosition:FieldLabelPosition = .Left {
+        didSet {
+            self.setupConstraints()
+            self.stylize()
+        }
+    }
+    
+    public var title:String? {
+        didSet {
+            self.update()
+        }
+    }
+    
+    func formattedTitle() -> String? {
+        if self.labelPosition == .Top {
+            return self.title?.uppercaseString
+        } else {
+            return self.title
+        }
+    }
+    
+    override public func buildView() {
+        super.buildView()
+        
+        let mainContent = UIView(frame: CGRect.zero)
+        mainContent.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(mainContent)
+        self.mainContent = mainContent
+        
+        let detailContent = UIView(frame: CGRect.zero)
+        detailContent.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(detailContent)
+        self.detailContent = detailContent
+        
+        let titleLabel = UILabel(frame: CGRect.zero)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.mainContent?.addSubview(titleLabel)
+        self.titleLabel = titleLabel
+        
+        let controlView = UIStackView(frame: CGRect.zero)
+        controlView.axis = UILayoutConstraintAxis.Horizontal
+        controlView.translatesAutoresizingMaskIntoConstraints = false
+        controlView.spacing = 15
+        self.mainContent?.addSubview(controlView)
+        self.controlView = controlView
+        
+        
+        let views = ["main":mainContent, "detail":detailContent]
+        let metrics = [
+            "left": self.defaultContentInsets.left,
+            "right": self.defaultContentInsets.right,
+            "top": self.defaultContentInsets.top,
+            "bottom": self.defaultContentInsets.bottom,
+            "icon": 20
+        ]
+        
+        var constraints:[NSLayoutConstraint] = []
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[main]|", options: .AlignAllTop, metrics: metrics, views: views)
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|[detail]|", options: .AlignAllTop, metrics: metrics, views: views)
+        constraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|[main][detail]-bottom-|", options: .AlignAllLeft, metrics: metrics, views: views)
+        self.contentView.addConstraints(constraints)
+        
+        
+        self.setupConstraints()
+        self.stylize()
+        self.update()
+    }
+    
+    func update() {
+        self.titleLabel?.text = self.formattedTitle()
+    }
+    
+    override public func stylize() {
+        super.stylize()
+        
+        switch self.labelPosition {
+        case .Left:
+            self.titleLabel?.font = self.titleFont
+            self.titleLabel?.textColor = self.titleTextColor
+        case .Top:
+            self.titleLabel?.textColor = UIColor(white: 0.5, alpha: 1)
+            self.titleLabel?.font = UIFont.boldSystemFontOfSize(11)
+        }
+    }
+    
+    func setupConstraints() {
+        guard let titleLabel=self.titleLabel, controlView=self.controlView, mainContent=self.mainContent else { return }
+        
+        let views = ["title":titleLabel, "controls":controlView, "main":mainContent]
+        let metrics = [
+            "left": self.defaultContentInsets.left,
+            "right": self.defaultContentInsets.right,
+            "top": self.defaultContentInsets.top,
+            "bottom": self.defaultContentInsets.bottom,
+            "controlHeight": 10
+        ]
+        
+        for constraint in self.contentConstraints {
+            self.mainContent?.removeConstraint(constraint)
+        }
+        
+        var newConstraints:[NSLayoutConstraint] = []
+        
+        switch self.labelPosition {
+        case .Left:
+            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[title]-[controls]-right-|", options: .AlignAllTop, metrics: metrics, views: views)
+            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-top-[title]|", options: .AlignAllTop, metrics: metrics, views: views)
+            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-top-[controls]|", options: .AlignAllTop, metrics: metrics, views: views)
+        case .Top:
+            let options = NSLayoutFormatOptions.AlignAllLeft
+            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[title]-right-|", options: options, metrics: metrics, views: views)
+            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("H:|-left-[controls]-right-|", options: options, metrics: metrics, views: views)
+            newConstraints += NSLayoutConstraint.constraintsWithVisualFormat("V:|-top-[title]-[controls(>=controlHeight)]-|", options: options, metrics: metrics, views: views)
+        }
+        
+        self.mainContent?.addConstraints(newConstraints)
+        self.contentConstraints = newConstraints
+    }
+    
+    func addControl(control:UIView, alignment:ControlAlignment = .Right) {
+        control.translatesAutoresizingMaskIntoConstraints = false
+        self.controlView!.addArrangedSubview(control)
+    }
+    
+    override public func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        self.stylize()
+    }
+}
