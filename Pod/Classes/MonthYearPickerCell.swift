3d7f55a2b00f469aa8b00c3bcfc836639b9f710d
Sam Williams <samuel.williams@gmail.com>
2016-06-09 11:12:07 -0400

    update month year picker when setting value

--- Pod/Classes/MonthYearPickerCell.swift
+++ Pod/Classes/MonthYearPickerCell.swift
@@ -32,6 +32,7 @@ public class MonthYearPickerCell: PickerCell {
         }
         set {
             self.monthYearPicker?.dateComponents = newValue
+            self.update()
         }
     }
     
@@ -41,6 +42,7 @@ public class MonthYearPickerCell: PickerCell {
         }
         set {
             self.monthYearPicker?.date = newValue
+            self.update()
         }
     }
     
--- PrimarySource.podspec
+++ PrimarySource.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = "PrimarySource"
-  s.version          = "0.9.12"
+  s.version          = "0.9.13"
   s.summary          = "Collection data sources"
   s.homepage         = "https://github.com/sadawi/PrimarySource"
   s.license          = 'MIT'
