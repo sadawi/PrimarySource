aec82e2980479ebb8ed557c728d807c9d493b29c
Sam Williams <samuel.williams@gmail.com>
2016-12-03 01:03:53 -0500

    ActionItem is a struct

--- Pod/shared/Classes/ActionList.swift
+++ Pod/shared/Classes/ActionList.swift
@@ -11,10 +11,10 @@ import Foundation
 public protocol Color {
 }
 
-open class ActionItem {
-    open var title: String?
-    open var action: (()->())?
-    open var color: Color?
+public struct ActionItem {
+    public var title: String?
+    public var action: (()->())?
+    public var color: Color?
     
     public init(title: String, color: Color?=nil, action: @escaping (()->())) {
         self.title = title
@@ -23,10 +23,14 @@ open class ActionItem {
     }
 }
 
-open class ActionList {
-    var actionItems: [ActionItem] = []
+public class ActionList {
+    var actionItems: [ActionItem]
     
-    func add(actionItem: ActionItem?) {
+    public init(items: [ActionItem]=[]) {
+        self.actionItems = items
+    }
+    
+    public func add(actionItem: ActionItem?) {
         if let actionItem = actionItem {
             self.actionItems.append(actionItem)
         }
